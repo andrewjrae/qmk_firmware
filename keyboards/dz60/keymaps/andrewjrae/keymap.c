8f8fb5e2665c5df484ad1bd9baa9239aaabdb6b0
Andrew Rae <ajrae.nv@gmail.com>
2021-02-10 09:05:53 -0500

    Changed removed num layer tap on space for gaming.

--- keyboards/dz60/keymaps/andrewjrae/keymap.c
+++ keyboards/dz60/keymaps/andrewjrae/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                               KC_TAB,     KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,       KC_Y,       KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
                               LT(NAV_L, KC_ESC), KC_A,    KC_S,    KC_D,   KC_F,    KC_G,       KC_H,       KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
                               MY_LSFT,    KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,       KC_N,       KC_M,     KC_COMM, KC_DOT,  KC_SLSH, MY_RSFT,
-                              MY_LCTL,    MY_LGUI, MY_LALT, MY_SPC, MY_RALT, OSL(NAV_L), OSL(NAV_L), MY_RCTL),
+                              MY_LCTL,    MY_LGUI, MY_LALT, KC_SPC, MY_RALT, OSL(NAV_L), OSL(NAV_L), MY_RCTL),
 
     [NAV_L] = LAYOUT_60_ansi(MY_ESC,      KC_F1,   KC_F2,   KC_F3,  KC_F4,   KC_F5,      KC_F6,      KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
                              KC_TRNS,     KC_NO,   VI_W,    KC_END, KC_NO,   KC_NO,      KC_NO,      KC_NO,    KC_HOME, KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_TRNS,
